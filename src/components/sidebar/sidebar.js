799c170cb4bb910f2213f9c7efd0c63467ceb717
letdevx <leticiasistemasads@gmail.com>
2023-11-10 23:58:43 -0300

    sidebar

---
+++ src/components/sidebar/sidebar.js
@@ -0,0 +1,45 @@
+import { Link } from "react-router-dom";
+import logo from './dente.png'
+import "./sidebar.css"
+
+function Sidebar(){
+    return (
+        <div className="d-flex" id="wrapper">
+          {/* Barra lateral */}
+          <nav className="col-md-2  d-md-block bg-dark sidebar">
+            <div className="sidebar-sticky align-items-center">
+              <div>
+                <img className="logo-dente" src={logo}/>
+              </div>  
+              <ul className="nav flex-column">
+                <li className="nav-item">
+                  <a className="nav-link active" href="#">
+                    Item 1
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Item 2
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Item 3
+                  </a>
+                </li>
+                {/* Adicione mais itens conforme necessário */}
+              </ul>
+            </div>
+          </nav>
+    
+          {/* Conteúdo principal */}
+          <main role="main" className="col-md-9 ml-sm-auto col-lg-10 px-4">
+            {/* Seu conteúdo principal vai aqui */}
+            <h1 className="mt-2">Conteúdo Principal</h1>
+          </main>
+        </div>
+      );
+    }
+  
+
+export default Sidebar; 
