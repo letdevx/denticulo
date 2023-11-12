47ffb60447569afd275ca79c363d9284e1e8e6aa
letdevx <leticiasistemasads@gmail.com>
2023-11-12 19:09:18 -0300

    Adicionado o middleware cors.

--- back/app.js
+++ back/app.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var logger = require('morgan');
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
