4c8ae42be478fb0b92a7f6cc58d24c70a8fa8786
leticia ast <leticia.sistemasads@gmail.com>
2023-11-14 01:07:16 -0300

    feito o tratamento de eventos no cadastro

--- front/src/pages/cadastro.js
+++ front/src/pages/cadastro.js
@@ -1,9 +1,23 @@
+import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import Dente from "../assets/gravura.png"
-import "./cadastro.css"
+import Dente from "../assets/gravura.png";
+import "./cadastro.css";
 
 const Cadastro = () => {
+    const [inputs, setInputs] = useState({});
+
+    const handleChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setInputs(values => ({ ...values, [name]: value }))
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        alert(`Muito bem ${inputs.nome}`);
+    }
+
     return (
         <div className="container-fluid h-100 bg-primary">
             <br></br>
@@ -12,39 +26,39 @@ const Cadastro = () => {
                 <div className="col-6  text-left my-auto">
                     <img className="ilustracao" src={Dente} alt="dente" />
                 </div>
-                <Form className="col-6">
+                <Form className="col-6" onSubmit={handleSubmit}>
                     <div className="box col-8">
                         <div>
-                            <h2 className= "text-center">Odontologia</h2>
-                            <h4 className= "text-center"> É hora de cuidar do seu sorriso </h4>
+                            <h2 className="text-center">Odontologia</h2>
+                            <h4 className="text-center"> É hora de cuidar do seu sorriso </h4>
                         </div>
                         <Form.Group className="mb-3" controlId="formNome">
                             <Form.Label>Nome completo</Form.Label>
-                            <Form.Control type="text" placeholder="Nome completo" />
+                            <Form.Control type="text" name="nome" value={inputs.nome || ""} placeholder="Nome completo" onChange={handleChange} />
                         </Form.Group>
 
                         <Form.Group className="mb-3" controlId="formEmail">
                             <Form.Label>Email</Form.Label>
-                            <Form.Control type="email" placeholder="Email" />
+                            <Form.Control type="email" name="email" value={inputs.email || ""} placeholder="Email" onChange={handleChange}/>
                         </Form.Group>
-                       
+
                         <Form.Group className="mb-3" controlId="formTelefone">
                             <Form.Label>Telefone</Form.Label>
-                            <Form.Control type="email" placeholder="Telefone" />
+                            <Form.Control type="text" name="telefone" value={inputs.telefone || ""} placeholder="Telefone" onChange={handleChange} />
                         </Form.Group>
 
                         <Form.Group className="mb-3" controlId="formCpf">
                             <Form.Label>CPF</Form.Label>
-                            <Form.Control type="text" placeholder="CPF" />
+                            <Form.Control type="text" name="cpf" value={inputs.cpf || ""} placeholder="CPF" onChange={handleChange} />
                         </Form.Group>
-                       
+
                         <Button variant="primary" type="submit">
                             Agende sua consulta
                         </Button>
                     </div>
                 </Form>
             </div>
-            <br/>
+            <br />
         </div>
     )
 };
