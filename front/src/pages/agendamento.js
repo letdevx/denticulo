ecbb3916f834a35cb6f9117372827bb5669b5d5d
letdevx <leticiasistemasads@gmail.com>
2023-11-14 20:11:35 -0300

    Corrigidos os selects na tela de agendamento.

--- front/src/pages/agendamento.js
+++ front/src/pages/agendamento.js
@@ -4,35 +4,35 @@ import RestApiService from '../services/restApiService';
 
 function Agendamento() {
     const [dentistas, setDentistas] = useState([]);
-    const [dentista, setDentista] = useState({});
+    const [idDentista, setIdDentista] = useState("");
 
     const [especialidades, setEspecialidades] = useState([]);
-    const [especialidade, setEspecialidade] = useState({});
+    const [idEspecialidade, setIdEspecialidade] = useState("");
     
     useEffect(() => {
         async function getAllDentistas() {
             const api = new RestApiService('http://localhost:8000/dentistas');
             let response = await api.getAllAsync();
             setDentistas(response);
-            setDentista(response[0]);
+            setIdDentista(response[0]._id);
         }
         async function getAllEspecialidades(){
             const api = new RestApiService('http://localhost:8000/especialidades');
             let response = await api.getAllAsync();
             setEspecialidades(response);
-            setEspecialidade(response[0]);
+            setIdEspecialidade(response[0]._id);
         }
         getAllDentistas();
         getAllEspecialidades();
     }, []);
 
     const handleChange = (event) => {
-        setDentista(event.target.value);
+        setIdDentista(event.target.value);
     };
 
     // funcao que faz a mmudanca na entidade especialidade
-    const tratarMudancaEspecialidade =(event) => {
-        setEspecialidade(event.target.value);
+    const tratarMudancaEspecialidade = (event) => {
+        setIdEspecialidade(event.target.value);
     };
 
     return (
@@ -49,7 +49,7 @@ function Agendamento() {
                             <h2>Selecione o seu Dentista:</h2>
                         </div>
                         <div className="seletor">
-                            <select className="form-select" aria-label="Default select example" value={dentista._id || ""} onChange={handleChange}>
+                            <select className="form-select" aria-label="Default select example" value={idDentista} onChange={handleChange}>
                                 {dentistas.map((d, i) => (<option key={i} value={d._id}>{d.nome}</option>))}
                             </select>
                             <br></br>
@@ -61,16 +61,14 @@ function Agendamento() {
                     <div>
                         <h2>Escolha seu procedimento:</h2>
                     </div>
-                    <select className="form-select" aria-label="Default select example" value={especialidade._id || ""} onChange={tratarMudancaEspecialidade}>
+                    <select className="form-select" aria-label="Default select example" value={idEspecialidade} onChange={tratarMudancaEspecialidade}>
                         {especialidades.map((e, i) => (<option key={i} value={e._id}>{e.descricao}</option>))}    
                     </select>
                     <br></br>
                 </div>
                 
-
                 <ModalConfirmacao/>
             </div>
-
         </>
     );
 }
